3108d42cc3007acf7e477546fa06732d03deafaf
vnikonov63 <nikonov.va@yandex.ru>
2020-09-03 14:28:48 +0300

    Release heroku fix

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const peerServer = PeerServer({
   // path: "/myapp",
 });
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
