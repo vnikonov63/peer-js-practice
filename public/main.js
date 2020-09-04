0815492c34422faab2e3b047b5f418b8c8c9d399
vnikonov63 <nikonov.va@yandex.ru>
2020-09-04 16:31:52 +0300

    FFF

--- public/main.js
+++ public/main.js
@@ -11,6 +11,7 @@ let userCount = 1;
 navigator.mediaDevices
   .getUserMedia({
     video: { width: 150, height: 150 },
+    adudio: true,
   })
   .then((stream) => {
     addVideoStream(myVideo, stream);
